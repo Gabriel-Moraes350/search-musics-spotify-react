d9040643563cc94b3f8ec9ef065fcdf0943f2c26
Gabriel Moraes <gabriel.m.baptista@gmail.com>
2019-09-08 14:40:21 -0300

    Updates

---
+++ precache-manifest.16fe2dfd206bc11f47c1fdfb04349b69.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fca85a8ed3bb93196d0d36cf71ac049",
+    "url": "/search-musics-spotify-react/index.html"
+  },
+  {
+    "revision": "aca3070a1f84de448f9c",
+    "url": "/search-musics-spotify-react/static/css/2.b1119605.chunk.css"
+  },
+  {
+    "revision": "aca3070a1f84de448f9c",
+    "url": "/search-musics-spotify-react/static/js/2.7aabe0ba.chunk.js"
+  },
+  {
+    "revision": "0441d32b0eeb8e115b10",
+    "url": "/search-musics-spotify-react/static/js/main.b5cf1dd3.chunk.js"
+  },
+  {
+    "revision": "e1aaa19a97a29acaf53c",
+    "url": "/search-musics-spotify-react/static/js/runtime~main.b1ad286d.js"
+  },
+  {
+    "revision": "fb0f946bd3160c3e60dfc153553bc01a",
+    "url": "/search-musics-spotify-react/static/media/home.fb0f946b.png"
+  },
+  {
+    "revision": "9303e2413f7b52e910b9359cdf7c3a3b",
+    "url": "/search-musics-spotify-react/static/media/logo.9303e241.png"
+  },
+  {
+    "revision": "32656c2e007b703cc2390c22f789e71d",
+    "url": "/search-musics-spotify-react/static/media/music.32656c2e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c068fc4efc1e961a4b2350a6bee2a21d.js"
+  "/search-musics-spotify-react/precache-manifest.16fe2dfd206bc11f47c1fdfb04349b69.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/search-musics-spotify-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
